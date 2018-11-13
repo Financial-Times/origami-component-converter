7ea031acaf78ae348c1798e9818c1026f89b41a0
chee <chee@snoot.club>
2018-11-13 20:14:05 +0000

    sort the components by their dependency needs before operating (by default)

--- source/library/components.js
+++ source/library/components.js
@@ -5,8 +5,13 @@ import spawn from './spawn.js'
 import compose from './compose.js'
 import rootManifest from './root-manifest.js'
 import * as root from './root.js'
+import * as bower from './bower.js'
+import toposort from 'toposort'
+import {keys} from './dictionary.js'
+import unary from './unary.js'
+import splitEvery from './split-every.js'
 
-export let names: string[] = Object.keys(rootManifest.dependencies)
+export let names: string[] = keys(rootManifest.dependencies)
 
 export let componentsDirectory = root.resolve(settings.componentsDirectory)
 
@@ -18,20 +23,25 @@ export let resolve = (componentName: string, ...files?: string[]): string => {
 	)
 }
 
-export function map (fn: string => any): Promise<void | any> {
-	let perform = name => fn(name)
-	return Promise.all(names.map(perform))
+export function map (fn: string => any, componentNames: string[] = sort()): Promise<void | any> {
+	let perform = name => unary(fn)(name)
+	return Promise.all(componentNames.map(perform))
 }
 
-export function mapDirectories (fn: string => any): Promise<void | any> {
-	let perform = compose(fn, resolve)
-	return Promise.all(names.map(perform))
+export function mapDirectories (fn: string => any, componentNames: string[] = sort()): Promise<void | any> {
+	let perform = compose(fn, unary(resolve))
+	return Promise.all(componentNames.map(perform))
 }
 
 export function spawnEach (command: string): Promise<void | any> {
 	return mapDirectories(cwd => spawn(command, {cwd}))
 }
 
-export function sort () {
-	return names
-}
+export let sort = (componentNames?: string[] = names): string[] =>
+	toposort(componentNames.reduce((graph, componentName) =>
+		graph.concat(
+			bower.getAllDependencyNames(bower.getManifest(componentName))
+				.filter(componentName => names.includes(componentName))
+				.map(dependencyName => [dependencyName, componentName])
+		), []
+	))
