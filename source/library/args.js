0ba5566ab14baa82f36b4bfdc0ab7e37313ba9d7
chee <chee@snoot.club>
2018-11-15 14:16:11 +0000

    use yargs for argument parsing

--- source/library/args.js
+++ source/library/args.js
@@ -1,8 +1,78 @@
-let args = [].reduce.call(process.argv.slice(2).filter(argument => argument.startsWith('-')).join(''), (args, argument) => {
-	args[argument] = args[argument] ? args[argument] + 1 : true
-	return args
-}, {})
+// @flow
+import yargs from 'yargs'
+import settings from './settings.js'
+import path from 'path'
+import defaultComponentNames from './component-names.js'
 
-delete args['-']
+let parseComponents = (components: string | Array<string>): string[] =>
+	Array.isArray(components)
+		? components
+		: components.split(/,/)
 
-export default args
+export default yargs
+	.count('verbose')
+	.alias('v', 'verbose')
+	.options('fresh', {
+		describe: 'delete old components/ directory',
+		alias: 'f',
+		default: false,
+		type: 'boolean'
+	})
+	.options('download', {
+		describe: 'do a bower install -F',
+		default: true,
+		type: 'boolean'
+	})
+	.options('create-manifests', {
+		describe: 'create package.jsons from .bower.jsons',
+		alias: 'n',
+		default: true,
+		type: 'boolean'
+	})
+	.options('create-links', {
+		describe: 'create global npm links',
+		default: false,
+		type: 'boolean'
+	})
+	.options('build', {
+		describe: 'build the components',
+		default: true,
+		type: 'boolean'
+	})
+	.options('clean-manifest', {
+		describe: 'clean the manifest before publish',
+		default: true,
+		type: 'boolean'
+	})
+	.options('publish', {
+		alias: 'p',
+		describe: 'publish to npm registry',
+		default: false,
+		type: 'boolean'
+	})
+	.options('prerelease', {
+		describe: 'prerelease tag to use (not implemented)',
+		default: settings.prerelease
+	})
+	.options('organisation', {
+		describe: 'organisation to use (not implemented)',
+		default: settings.organisation
+	})
+	.options('print-components', {
+		describe: 'show components in sort order and die'
+	})
+	.options('working-directory', {
+		default: process.cwd(),
+		coerce: (directory: string) => path.resolve(process.cwd(), directory)
+	})
+	.options('components', {
+		describe: 'the components to use',
+		default: defaultComponentNames,
+		coerce: parseComponents
+	})
+	.options('npm-install', {
+		default: true,
+		type: 'boolean',
+		describe: 'install component dependencies'
+	})
+	.argv
