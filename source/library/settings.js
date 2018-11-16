930c3bea05523fc5c2e7f6f00ad976b7022729ed
chee <chee@snoot.club>
2018-11-16 10:31:57 +0000

    remove settings.js curses

--- source/library/settings.js
+++ source/library/settings.js
@@ -14,32 +14,6 @@ export type Settings = {
 	prerelease: string | false
 }
 
-import semver from 'semver'
 import importJson from './import-json.js'
-import read from './read-object.js'
-import write from './write-object.js'
-
 let settings: Settings = importJson('settings.json')
-
-/** ooh it mutates a boi isn't that nasty */
-let updateSettings = () =>
-	read('settings.json').then(next => settings = next)
-
-type SemverLevels = 'major' | 'minor' | 'patch'
-
-export let bumpPrerelease = (level?: SemverLevels = 'patch'): Promise<void> => {
-	if (!settings.prerelease) {
-		return Promise.reject('it bad')
-	}
-
-	let prerelease = semver.inc(settings.prerelease, level)
-
-	return write('settings.json', {
-		...settings,
-		prerelease
-	})
-		.then(updateSettings)
-		.then(() => new Promise(resolve => setTimeout(resolve, 2000)))
-}
-
 export default settings
