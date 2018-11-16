8ce94d8385d0805212df4b0bf0a8af4b2a0f1552
chee <chee@snoot.club>
2018-11-16 09:55:47 +0000

    add names.{targets,all,origami} to components.js

--- source/library/components.js
+++ source/library/components.js
@@ -3,37 +3,65 @@
 import settings from './settings.js'
 import spawn from './spawn.js'
 import compose from './compose.js'
-import rootManifest from './root-manifest.js'
-import * as root from './root.js'
+import args from './args.js'
 import * as bower from './bower.js'
 import toposort from 'toposort'
-import {keys} from './dictionary.js'
 import unary from './unary.js'
 import splitEvery from './split-every.js'
+import path from 'path'
+import componentNames from './component-names.js'
+import * as workingDirectory from './working-directory.js'
 
-export let names: string[] = keys(rootManifest.dependencies)
+export let targetNames: string[] = args.components
 
-export let componentsDirectory = root.resolve(settings.componentsDirectory)
+type Names = {
+	targets: string[],
+	all: string[],
+	origami: string[]
+}
 
-export let resolve = (componentName: string, ...files?: string[]): string => {
-	return root.resolve(
+export let names: Names = {
+	targets: args.components,
+	origami: componentNames,
+	all: Array.from(new Set([...targetNames, ...componentNames]))
+}
+
+export let includes = (componentName: string): boolean =>
+	names.all.includes(componentName)
+
+export let componentsDirectory = workingDirectory.resolve(
+	settings.componentsDirectory
+)
+
+export let resolve = (componentName: string, ...files?: string[]): string =>
+	path.resolve(
 		componentsDirectory,
 		componentName,
 		...files || []
 	)
-}
 
-export function map (fn: string => any, componentNames: string[] = sort()): Promise<void | any> {
-	let perform = name => unary(fn)(name)
+export async function map (fn: string => any, componentNames?: string[]): Promise<void | any> {
+	componentNames = componentNames || await sort()
+	let perform = name => fn(name)
 	return Promise.all(componentNames.map(perform))
 }
 
-export function mapDirectories (fn: string => any, componentNames: string[] = sort()): Promise<void | any> {
+export async function sequence (fn: string => any, componentNames?: string[]): Promise<void | any> {
+	componentNames = componentNames || await sort()
+	let perform = (promise: Promise<void>, name) =>
+		promise.then(() => fn(name))
+
+	return componentNames.reduce(perform, Promise.resolve())
+}
+
+export async function mapDirectories (fn: string => any, componentNames?: string[]): Promise<void | any> {
+	componentNames = componentNames || await sort()
 	let perform = compose(fn, unary(resolve))
 	return Promise.all(componentNames.map(perform))
 }
 
-export function batch (command: string | ?string => string | false, componentNames?: string[] = sort(), batchSize?: number = 8): Promise<void | any> {
+export async function batch (command: string | ?string => string | false, componentNames?: string[], batchSize?: number = 8): Promise<void | any> {
+	componentNames = componentNames || await sort()
 	let getCommand = (name?: string) =>
 		typeof command == 'function'
 			? command(name)
