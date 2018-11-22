27e178f799c3ca31d62b592fbda683abd9fb7290
chee <chee@snoot.club>
2018-11-22 19:24:15 +0000

    add bin directory to workingDirectory

--- source/library/working-directory.js
+++ source/library/working-directory.js
@@ -3,3 +3,5 @@ import args from './args.js'
 import createResolver from './create-resolver.js'
 
 export let resolve = createResolver(args.workingDirectory)
+
+export let binaryDirectory = resolve('node_modules', '.bin')
