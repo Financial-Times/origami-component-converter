6f11fc4f4ff5f597d752526855c9dcb22b01f028
chee <chee@snoot.club>
2019-01-07 13:38:13 +0000

    use fs-extra's readJson

--- source/library/read-object.js
+++ source/library/read-object.js
@@ -1,19 +1,7 @@
 // @flow
 import * as fs from 'fs-extra'
 
-import compose from './compose.js'
 import * as workingDirectory from './working-directory.js'
 
-let parse = async (json: Promise<string>): Promise<any> =>
-	JSON.parse(await json)
-
-let read = async (filename: string): Promise<string> =>
-	(await fs.readFile(
-		workingDirectory.resolve(filename),
-		{encoding: 'utf-8'}
-	)).toString()
-
-let readObject: (string => Promise<any>) =
-	compose(parse, read)
-
-export default readObject
+export default async (filename: string): Promise<any> =>
+	fs.readJson(workingDirectory.resolve(filename))
