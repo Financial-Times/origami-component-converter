094f4b09d2f8f91070d14e0fac32d36b2687ed2e
chee <chee@snoot.club>
2019-01-07 13:57:28 +0000

    use fs commands for copyPackageJson (less entanglements)

--- source/library/working-directory.js
+++ source/library/working-directory.js
@@ -1,14 +1,13 @@
 // @flow
 import yargs from 'yargs'
 import createResolver from './create-resolver.js'
-import write from './write-object.js'
-import read from './read-object.js'
+import fs from 'fs-extra'
 import * as root from './root.js'
 
-export let copyPackageJson = async () => {
-	let rootManifest = await read(root.resolve('package.json'))
-	rootManifest.name = Math.random().toString(36).slice(2)
-	return write(yargs.argv.workingDirectory, 'package.json', rootManifest)
-}
+export let resolve = (...args) => createResolver(yargs.argv.workingDirectory)(...args)
 
-export let resolve = createResolver(yargs.argv.workingDirectory)
+export let copyPackageJson = async () => {
+	let rootManifest = await fs.readJson(root.resolve('package.json'))
+	rootManifest.name = '~nothing~'
+	return fs.outputJson(resolve('package.json'), rootManifest)
+}
