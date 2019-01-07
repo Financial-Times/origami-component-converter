27afb94a426519356168ed7449908b3bee95e330
chee <chee@snoot.club>
2019-01-07 13:19:15 +0000

    make the working directory when we begin

--- source/index.js
+++ source/index.js
@@ -2,8 +2,24 @@
 // @flow
 require('dotenv').config()
 import yargs from 'yargs'
+import path from 'path'
+import os from 'os'
+import {mkdirp} from 'fs-extra'
 
-export default yargs
+let {argv} = yargs
+	.showHelpOnFail(true, '🐕')
+	.wrap(yargs.terminalWidth())
 	.commandDir('./commands')
+	.options('working-directory', {
+		global: true,
+		default: path.resolve(os.homedir(), 'tmp/origami'),
+		coerce: (directory: string) => path.resolve(process.cwd(), directory)
+	})
+	.epilog('🐕')
 	.demandCommand()
-	.argv
+	.help('h')
+	.alias('h', 'help')
+
+mkdirp(argv.workingDirectory)
+
+export default argv
