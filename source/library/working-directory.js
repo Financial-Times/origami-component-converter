380d621b246c557f5263ea2336c4eacc62febab3
chee <chee@snoot.club>
2019-01-13 17:49:01 +0000

    Add type annotation for args

--- source/library/working-directory.js
+++ source/library/working-directory.js
@@ -1,10 +1,10 @@
 // @flow
 import yargs from 'yargs'
 import createResolver from './create-resolver.js'
-import fs from 'fs-extra'
+import * as fs from 'fs-extra'
 import * as root from './root.js'
 
-export let resolve = (...args) => createResolver(yargs.argv.workingDirectory)(...args)
+export let resolve = (...paths: string[]) => createResolver(yargs.argv.workingDirectory)(...paths)
 
 export let copyPackageJson = async () => {
 	let rootManifest = await fs.readJson(root.resolve('package.json'))
