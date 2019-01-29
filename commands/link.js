61aa734adaa436881bd04d3e768d1cfc42ff92f8
chee <chee@snoot.club>
2019-01-29 23:34:24 +0000

    Replace single use of unary with arrow function

--- commands/link.js
+++ commands/link.js
@@ -1,6 +1,5 @@
 import * as components from "../lib/components.js"
 import * as npm from "../lib/npm.js"
-import unary from "../lib/unary.js"
 
 export let command = "link"
 export let describe = "npm link all the deps globally"
@@ -10,7 +9,9 @@ export let handler = async function() {
 	await components.sequence(async name => {
 		if (!name) return false
 		let dependencies = await components.sort([name])
-		let names = dependencies.map(unary(npm.createComponentName)).join(" ")
+		let names = dependencies.map(name =>
+			npm.createComponentName(name)
+		).join(" ")
 
 		return Boolean(names.length) && `npm link ${names}`
 	})
