68e77317e01f15cbb55773111beabccd126b290d
chee <chee@snoot.club>
2019-02-05 19:32:13 +0000

    Remove unused quiet opt

--- commands/get-latest-tag.js
+++ commands/get-latest-tag.js
@@ -1,15 +1,14 @@
 import * as github from "../lib/github.js"
 
 export let describe = "fetch the latest tagged release number"
+
 export let command = "get-latest-tag <component>"
+
 export let builder = yargs =>
 	yargs
 		.positional("component", {
 			default: false,
 			describe: "component name"
-		})
-		.option("quiet", {
-			default: true
 		})
 
 export async function handler ({component}) {
