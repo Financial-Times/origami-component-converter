0c35287f613429655bf0481e481c8753cc60a43e
chee <chee@snoot.club>
2019-02-05 19:29:20 +0000

    Add a command for getting the latest tag

---
+++ commands/get-latest-tag.js
@@ -0,0 +1,17 @@
+import * as github from "../lib/github.js"
+
+export let describe = "fetch the latest tagged release number"
+export let command = "get-latest-tag <component>"
+export let builder = yargs =>
+	yargs
+		.positional("component", {
+			default: false,
+			describe: "component name"
+		})
+		.option("quiet", {
+			default: true
+		})
+
+export async function handler ({component}) {
+	console.info(await github.getLatestTag(component))
+}
