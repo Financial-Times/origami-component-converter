51d5bd82e5e8f24fd7f9137b59ff2e20d7b5b936
chee <chee@snoot.club>
2019-02-08 18:34:39 +0000

    Add a command that filters bower to npm manifest

---
+++ commands/convert-manifest.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import * as npm from "../lib/npm.js"
+import {EOL} from "os"
+export let command = "convert-manifest"
+export let desc =
+	"read a bower manifest on stdin and write a npm manifest to stdout"
+
+export let builder = yargs =>
+	yargs
+		.option("semver", {
+			describe: "the version number to use in the package.json",
+			type: "string",
+			required: true
+		})
+		.option("include-babel-config", {
+			describe: "whether or not to include babel config in the package.json",
+			type: "boolean",
+			default: false
+		})
+
+let {
+	stdin,
+	stdout
+} = process
+
+export let handler = async function ဪ(argv) {
+	let input = ""
+
+	stdin.resume()
+	stdin.setEncoding("utf-8")
+
+	stdin.on("data", chunk => input += chunk)
+
+	stdin.on("end", async () => {
+		let bowerManifest= JSON.parse(input)
+		bowerManifest.version = argv.semver
+		let npmManifest = await npm.createManifest(bowerManifest)
+		if (!argv.includeBabelConfig) {
+			delete npmManifest.babel
+		}
+		stdout.write(JSON.stringify(npmManifest, null, "\t") + EOL)
+	})
+}
