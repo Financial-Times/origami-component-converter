283980a0ab009ccb1f5ddfbcb0e67f0ce0272ceb
chee <chee@snoot.club>
2019-02-11 21:42:25 +0000

    Make `semver` positional and `directory` an option

--- commands/build.js
+++ commands/build.js
@@ -3,21 +3,21 @@ import * as npm from "../lib/npm.js"
 import * as babel from "../lib/babel.js"
 import * as fs from "fs-extra"
 import path from "path"
-import writeObject from "../lib/write-object.js"
 
-export let command = "build [directory]"
+export let command = ["$0 <semver>", "build <semver>"]
 export let desc = "build a component in place"
 
 export let builder = yargs =>
 	yargs
-		.positional("directory", {
+		.option("cwd", {
+			alias: ["directory"],
 			describe: "the directory to build",
 			default: process.cwd(),
 			coerce (directory) {
 				return path.resolve(process.cwd(), directory)
 			}
 		})
-		.option("semver", {
+		.positional("semver", {
 			describe: "the version number to use in the package.json",
 			type: "string",
 			required: true
