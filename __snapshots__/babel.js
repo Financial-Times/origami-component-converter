8eeb53a496424dc74a7ab9715b786d4ba51f699b
chee <chee@snoot.club>
2019-02-18 07:01:41 +0000

    Update snapshot

--- __snapshots__/babel.js
+++ __snapshots__/babel.js
@@ -2,21 +2,13 @@ exports['babel config'] = {
   "filename": "",
   "presets": [],
   "plugins": [
-    [
-      "~root~/node_modules/babel-plugin-module-resolver",
-      {
-        "alias": {
-          "alice": "banana",
-          "charlie": "delta"
-        }
-      }
-    ]
+    null
   ],
   "overrides": [
     {
       "presets": [
         [
-          "~root~/node_modules/@babel/preset-env",
+          "/Users/chee/Projects/Origami/occ/node_modules/@babel/preset-env",
           {
             "useBuiltIns": false,
             "targets": {
@@ -27,55 +19,23 @@ exports['babel config'] = {
         ]
       ],
       "plugins": [
-        "~root~/node_modules/babel-plugin-add-module-exports",
-        [
-          "~root~/node_modules/babel-plugin-import-redirect",
-          {
-            "redirect": {
-              "alice/src/(.*)": "banana/dist/$1",
-              "charlie/src/(.*)": "delta/dist/$1"
-            },
-            "suppressResolveWarning": true
-          }
-        ]
+        "/Users/chee/Projects/Origami/occ/node_modules/babel-plugin-add-module-exports",
+        null
       ],
       "exclude": "./test.src/**"
     },
     {
       "presets": [],
       "plugins": [
-        "~root~/node_modules/babel-plugin-add-module-exports",
-        [
-          "~root~/node_modules/babel-plugin-import-redirect",
-          {
-            "redirect": {
-              "./src/(.*)": "./dist/$1",
-              "alice/src/(.*)": "banana/dist/$1",
-              "charlie/src/(.*)": "delta/dist/$1"
-            },
-            "suppressResolveWarning": true
-          }
-        ]
+        "/Users/chee/Projects/Origami/occ/node_modules/babel-plugin-add-module-exports",
+        null
       ],
       "test": "./main.js"
     },
     {
       "presets": [],
       "plugins": [
-        [
-          "~root~/node_modules/babel-plugin-import-redirect",
-          {
-            "root": "test",
-            "redirect": {
-              "../src/(.*)": "../dist/$1",
-              "./fixture/main": "./fixture/main",
-              "../main": "../browser",
-              "alice/src/(.*)": "banana/dist/$1",
-              "charlie/src/(.*)": "delta/dist/$1"
-            },
-            "suppressResolveWarning": true
-          }
-        ]
+        null
       ],
       "test": "./test.src/**"
     }
