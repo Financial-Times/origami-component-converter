b4418f7269d9650dbd39bcf69eeb4be440b6a2d8
notlee <lee.moody@ft.com>
2019-04-08 12:38:56 +0100

    Remove "ftdomdelegate" from component mappings.
    Versions 2.2.1 and above of "dom-delegate" have been published to
    NPM as "ftdomdelegate".
    
    https://www.npmjs.com/package/ftdomdelegate
    https://www.npmjs.com/package/dom-delegate

--- config/mappings.js
+++ config/mappings.js
@@ -1,7 +1,6 @@
 export default {
 	name: {
 		"ft-date-format": "@financial-times/ft-date-format",
-		ftdomdelegate: "dom-delegate",
 		prism: "prismjs",
 		pusher: "pusher-js",
 		hogan: "hogan.js"
