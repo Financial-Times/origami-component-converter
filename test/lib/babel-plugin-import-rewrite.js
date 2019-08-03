bcd515baeee38548886a40d638e2ea7dd91db2c6
Jake Champion <jakechampion.jake2@googlemail.com>
2019-08-03 16:10:05 +0100

    Switch from Facebook's expect to Chai

--- test/lib/babel-plugin-import-rewrite.js
+++ test/lib/babel-plugin-import-rewrite.js
@@ -4,53 +4,53 @@ import {
 	rewrite
 } from "../../lib/babel-plugin-import-rewrite.js"
 
-import expect from "expect"
+import { expect } from 'chai';
 
 describe("normalize", () => {
 	it("leaves bare paths alone", () => {
-		expect(normalize("x/x")).toBe("x/x")
+		expect(normalize("x/x")).to.eql("x/x")
 	})
 	it("leaves absolute paths alone", () => {
-		expect(normalize("/xx")).toBe("/xx")
+		expect(normalize("/xx")).to.eql("/xx")
 	})
 	it("leaves ./ paths alone", () => {
-		expect(normalize("./x")).toBe("./x")
+		expect(normalize("./x")).to.eql("./x")
 	})
 
 	it("leaves ../ paths alone", () => {
-		expect(normalize("../x")).toBe("../x")
+		expect(normalize("../x")).to.eql("../x")
 	})
 
 	it("fixes ./..", () => {
-		expect(normalize("./../x")).toBe("../x")
+		expect(normalize("./../x")).to.eql("../x")
 	})
 
 	it("removes noisy dots", () => {
-		expect(normalize("./.././../y")).toBe("../../y")
+		expect(normalize("./.././../y")).to.eql("../../y")
 	})
 })
 
 describe("replace", () => {
 	it("fixes main to browser", () => {
-		expect(replace("../../main.js")).toBe("../../browser.js")
+		expect(replace("../../main.js")).to.eql("../../browser.js")
 	})
 
 	it("fixes deeper main to deeper browser", () => {
-		expect(replace("../../../../main")).toBe("../../../../browser")
+		expect(replace("../../../../main")).to.eql("../../../../browser")
 	})
 
 	it("fixes src to dist", () => {
-		expect(replace("../src/lol")).toBe("../dist/lol")
+		expect(replace("../src/lol")).to.eql("../dist/lol")
 	})
 
 	it("fixes src to dist even on barebois", () => {
-		expect(replace("@x/src/lol")).toBe("@x/dist/lol")
+		expect(replace("@x/src/lol")).to.eql("@x/dist/lol")
 	})
 })
 
 describe("rewrite", () => {
 	it("normalizes", () => {
-		expect(rewrite("./..", {})).toBe("..")
+		expect(rewrite("./..", {})).to.eql("..")
 	})
 
 	let aliases = {
@@ -58,32 +58,32 @@ describe("rewrite", () => {
 	}
 
 	it("rewrites aliases", () => {
-		expect(rewrite("friend/lol", aliases)).toBe("@special/friend/lol")
+		expect(rewrite("friend/lol", aliases)).to.eql("@special/friend/lol")
 	})
 
 	it("leaves non-aliases items alone", () => {
-		expect(rewrite("o-colors/main", aliases)).toBe("o-colors/main")
+		expect(rewrite("o-colors/main", aliases)).to.eql("o-colors/main")
 	})
 
 	it("replaces src with dist in aliases", () => {
-		expect(rewrite("friend/src/lol", aliases)).toBe("@special/friend/dist/lol")
+		expect(rewrite("friend/src/lol", aliases)).to.eql("@special/friend/dist/lol")
 	})
 
 	it("replaces longer src paths with dist in aliases", () => {
 		expect(rewrite("friend/src/monkey/lol", aliases))
-			.toBe("@special/friend/dist/monkey/lol")
+			.to.eql("@special/friend/dist/monkey/lol")
 	})
 
 	it("replaces local main with local browser", () => {
-		expect(rewrite("../../../main", aliases)).toBe("../../../browser")
+		expect(rewrite("../../../main", aliases)).to.eql("../../../browser")
 	})
 
 	it("replaces local main.js with local browser.js", () => {
-		expect(rewrite("../../../main.js", aliases)).toBe("../../../browser.js")
+		expect(rewrite("../../../main.js", aliases)).to.eql("../../../browser.js")
 	})
 
 	it("replaces local src with local dist", () => {
-		expect(rewrite("../../src/lol", aliases)).toBe("../../dist/lol")
+		expect(rewrite("../../src/lol", aliases)).to.eql("../../dist/lol")
 	})
 })
 
