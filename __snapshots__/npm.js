8b67aad183c70fda2725882ff894b7475715caef
Jake Champion <me@jakechampion.name>
2020-02-04 13:15:16 +0000

    update npm snapshot as we have a new test

--- __snapshots__/npm.js
+++ __snapshots__/npm.js
@@ -1,4 +1,4 @@
-exports['createManifest creates the same manifest as last time 1'] = {
+exports['createManifest without a repository parameter creates the same manifest as last time 1'] = {
   "browser": "browser.js",
   "files": [
     "svg/",
@@ -9,6 +9,8 @@ exports['createManifest creates the same manifest as last time 1'] = {
     "main.scss",
     "img",
     "*.json",
+    "*.js",
+    "!main.js",
     "scss",
     "!bower.json",
     "!.bower.json"
@@ -29,5 +31,45 @@ exports['createManifest creates the same manifest as last time 1'] = {
     "prism": "prismjs",
     "pusher": "pusher-js",
     "hogan": "hogan.js"
+  }
+}
+
+exports['createManifest with a repository parameter creates the same manifest as last time 1'] = {
+  "browser": "browser.js",
+  "files": [
+    "svg/",
+    "dist/",
+    "browser.js",
+    "src/",
+    "!src/**/*.js",
+    "main.scss",
+    "img",
+    "*.json",
+    "*.js",
+    "!main.js",
+    "scss",
+    "!bower.json",
+    "!.bower.json"
+  ],
+  "eslintIgnore": [
+    "browser.js",
+    "dist/"
+  ],
+  "keywords": [
+    "origami",
+    "component",
+    "ft"
+  ],
+  "name": "@financial-times/o-test-component",
+  "component": "o-test-component",
+  "aliases": {
+    "ft-date-format": "@financial-times/ft-date-format",
+    "prism": "prismjs",
+    "pusher": "pusher-js",
+    "hogan": "hogan.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://origami.ft.com"
   }
 }
