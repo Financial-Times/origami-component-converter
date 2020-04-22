66fd0cf5fe57229bac6ffbb01610d8b5dfb2970f
chee <chee@snoot.club>
2020-04-22 12:51:25 +0100

    Automatically create snapshot for any directory in `components` dir

--- test/components.js
+++ test/components.js
@@ -2,45 +2,33 @@ import {dss as serializeDirectory} from "directory-snapshot"
 import snap from "snap-shot-it"
 import {handler as convertComponent} from "../commands/build"
 import {resolve as resolvePath} from "path"
+import fs from "fs"
 
-let components = [
-	["o-colors", "4.0.0"],
-	["o-forms", "7.0.0"],
-	["o-table", "7.0.0"],
-]
+let components = fs.readdirSync(resolvePath(__dirname, "components"))
 
-components.forEach(([name, semver]) => {
-	describe(`occ ${name}@${semver}`, () => {
+components.forEach(item => {
+	describe(`occ ${item}`, () => {
 		context("without CIRCLE_REPOSITORY_URL set", function() {
 			it("matches snapshot", async () => {
-				let directory = resolvePath(
-					__dirname,
-					"components",
-					`${name}-${semver}`
-				)
+
 				delete process.env.CIRCLE_REPOSITORY_URL
+				let directory = resolvePath(__dirname, "components", item)
 				await convertComponent({
 					directory,
-					semver,
+					semver: "0.0.0",
 				})
-				snap(serializeDirectory(directory))
+				snap(item, serializeDirectory(directory))
 			})
 		})
 
 		context("with CIRCLE_REPOSITORY_URL set", function() {
-			it("matches snapshot", async () => {
-				let directory = resolvePath(
-					__dirname,
-					"components",
-					`${name}-${semver}`
-				)
 				process.env.CIRCLE_REPOSITORY_URL = "https://origami.ft.com"
+				let directory = resolvePath(__dirname, "components", item)
 				await convertComponent({
 					directory,
-					semver,
+					semver: "0.0.0",
 				})
-				snap(serializeDirectory(directory))
+				snap(item, serializeDirectory(directory))
 			})
-		})
 	})
 })
