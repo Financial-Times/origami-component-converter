67ff55826626413dd659fdb6c3640cf0909eee8b
chee <chee@snoot.club>
2020-04-22 12:48:39 +0100

    Remove files array from default manifest

--- __snapshots__/npm.js
+++ __snapshots__/npm.js
@@ -1,21 +1,6 @@
 exports['createManifest without a repository parameter creates the same manifest as last time 1'] = {
   "browser": "browser.js",
-  "files": [
-    "svg/",
-    "dist/",
-    "browser.js",
-    "src/",
-    "!src/**/*.js",
-    "main.scss",
-    "img",
-    "*.json",
-    "*.js",
-    "!main.js",
-    "scss",
-    "!bower.json",
-    "!.bower.json"
-  ],
-  "eslintIgnore": [
+	"eslintIgnore": [
     "browser.js",
     "dist/"
   ],
