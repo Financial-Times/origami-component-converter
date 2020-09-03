1a441bb0b7f60a6bf1e435c633fd15ee525da5c2
Jake Champion <me@jakechampion.name>
2020-09-03 10:27:05 +0100

    Add optional repository  type definition onto the argv object

--- commands/build.js
+++ commands/build.js
@@ -42,6 +42,7 @@ export let builder = yargs =>
  * Take the bower.json from the directory, and generate a package.json (including aliases)
  * Then compile that javascript from `src` to `dist`, rewriting the aliases
  * Then remove the aliases from the manifest
+ * @param {{directory: string, semver: string, name:? string, repository:? string}} argv the arguments understood by yargs
  * @returns {Promise.<void>} promise which resolves when the npm version has been built
  */
 export let handler = async function build(argv) {
